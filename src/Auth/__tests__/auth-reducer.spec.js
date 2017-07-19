25f598926e47cab3491a9ae3787318e193eeaea6
Ruslan Abramov <r.o.abramov@gmail.com>
2017-07-19 18:06:15 +0300

    check if travis find error

--- src/Auth/__tests__/auth-reducer.spec.js
+++ src/Auth/__tests__/auth-reducer.spec.js
@@ -4,7 +4,7 @@ describe('User reducer', () => {
   it('Should be initialized with default value', () => {
     const action = { type: 'INIT' }
     const actualState = user(undefined, action)
-    const expectedState = null
+    const expectedState = 1
     expect(expectedState).toEqual(actualState)
   })
 })
