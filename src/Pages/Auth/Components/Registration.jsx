11de5fa08a2c6b7edfa26b1dd7c68a62948e4447
Ruslan Abramov <r.o.abramov@gmail.com>
2017-07-25 11:21:23 +0300

    Initial implementations of Validator HOC

--- src/Pages/Auth/Components/Registration.jsx
+++ src/Pages/Auth/Components/Registration.jsx
@@ -12,69 +12,110 @@ import {
   Label,
   Button
 } from '../../../Shared/Components/Controls'
-import {
-  withLogin,
-  withPassword,
-  handleLoginChange,
-  handlePasswordChange
-} from './helpers'
 import { AuthFormContainer, AuthFormTitle } from '../Styled.components'
+import { withValidation } from './Validation'
 
 const mapDispatchToProps = {
   doRegistration: registerUser
 }
 
-const handlers = withHandlers({
-  handleLoginChange,
-  handlePasswordChange,
-  performRegistration: ({ doRegistration, login, password, history }) => e => {
-    e.preventDefault()
-    doRegistration({ login, password })
+const config = {
+  login: {
+    validators: [
+      { rule: val => val.length > 3, message: 'Too short' },
+      { rule: val => val.startsWith('sn'), message: 'Must start with sn' }
+    ]
+  },
+  password: {
+    validators: [
+      {
+        rule: val => val.length > 4,
+        message: 'Password must be longer than 4 symbols'
+      }
+    ]
+  },
+  passwordConfirm: {
+    validators: [
+      {
+        rule: (val, password) => val === password,
+        message: 'Passwords not equal',
+        withFields: ['password']
+      }
+    ]
   }
-})
+}
 
 const enhancer = composeHOC(
   withRouter,
   connect(null, mapDispatchToProps),
-  withLogin,
-  withPassword,
-  handlers
+  withValidation(config)
 )
 
-export const RegistrationComponent = ({
-  performRegistration,
-  login,
-  handleLoginChange,
-  password,
-  handlePasswordChange
-}) =>
-  <AuthFormContainer>
-    <AuthFormTitle>Registration Component</AuthFormTitle>
-    <form noValidate={true} onSubmit={performRegistration}>
-      <FormGroup>
-        <Label htmlFor="login">Login</Label>
-        <Input
-          id="login"
-          type="text"
-          placeholder="login"
-          value={login}
-          onChange={handleLoginChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label htmlFor="password">Password</Label>
-        <Input
-          id="password"
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Button>Register</Button>
-      </FormGroup>
-    </form>
-  </AuthFormContainer>
+export class RegistrationComponent extends React.Component {
+  performRegistration = e => {
+    e.preventDefault()
+    this.props.doRegistration({
+      login: this.props.login.value,
+      password: this.props.password.value
+    })
+  }
+
+  displayErrors = key =>
+    this.props[key].errors.length !== 0
+      ? <div style={{ backgorundColor: 'red' }}>
+          {this.props[key].errors.map((e, i) =>
+            <div key={i}>
+              {e}
+            </div>
+          )}
+        </div>
+      : null
+
+  render() {
+    return (
+      <AuthFormContainer>
+        <AuthFormTitle>Registration Component</AuthFormTitle>
+        <form noValidate={true} onSubmit={this.performRegistration}>
+          {this.displayErrors('login')}
+          <FormGroup>
+            <Label htmlFor="login">Login</Label>
+            <Input
+              id="login"
+              type="text"
+              placeholder="login"
+              value={this.props.login.value}
+              onChange={this.props.login.handler}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="password"
+              value={this.props.password.value}
+              onChange={this.props.password.handler}
+            />
+            {this.displayErrors('password')}
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="passwordConfirm">Password Confirmation</Label>
+            <Input
+              id="passwordConfirm"
+              type="password"
+              placeholder="password confirmation"
+              value={this.props.passwordConfirm.value}
+              onChange={this.props.passwordConfirm.handler}
+            />
+            {this.displayErrors('passwordConfirm')}
+          </FormGroup>
+          <FormGroup>
+            {this.props.valid ? <Button>Register</Button> : null}
+          </FormGroup>
+        </form>
+      </AuthFormContainer>
+    )
+  }
+}
 
 export default enhancer(RegistrationComponent)
