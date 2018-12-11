6ea44762e57f9122d817b42cafc150c9a407005e
Ruslan Abramov <r.o.abramov@gmail.com>
2018-12-11 23:49:42 +0300

    remove dead components

--- src/Shared/Components/Controls/index.js
+++ src/Shared/Components/Controls/index.js
@@ -1,7 +1,5 @@
 export { Button } from './Button';
 export { FormGroup } from './Form.group';
-export { Input, TextArea } from './Input';
-export { Label } from './Label';
 export { SimpleLink, NavigationLink } from './Navigation.link';
 export { OutlineButton } from './Outline.button';
 export { CloseButton } from './CloseButton';
