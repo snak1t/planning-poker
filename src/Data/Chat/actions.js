de308397fc1440fa3b95fed60bb0eb4ef9280188
Ruslan Abramov <Ruslan_Abramov@epam.com>
2018-12-14 19:05:01 +0300

    feat(chat): refactor char reducer into hook implementation

--- src/Data/Chat/actions.js
+++
@@ -1,8 +0,0 @@
-import socketConst from '../../socket.constants'
-
-export const emitMessage = payload => {
-  return {
-    type: socketConst.MESSAGE_RECEIVED,
-    payload
-  }
-}
--- src/Data/store.js
+++ src/Data/store.js
@@ -5,17 +5,15 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 //reducers
-import { chatMessages } from './Chat/reducer';
 import { stories } from './Stories/reducer';
 import games from './Games/reducer';
 import playSession from './PlaySession/reducer';
 
 import openSocket from 'socket.io-client';
-const socket = openSocket('/');
+export const socket = openSocket('/');
 const middlewares = [thunk, createSocketIoMiddleware(socket, '[sockets] ')];
 
 const reducers = {
-    chatMessages,
     stories,
     games,
     playSession,
