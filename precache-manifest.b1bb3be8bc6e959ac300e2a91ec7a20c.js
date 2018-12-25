49f9590a20aa02b25494058b9c4465667d579019
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-25 12:31:51 +0000

    Deploy snak1t/planning-poker to github.com/snak1t/planning-poker.git:gh-pages

---
+++ precache-manifest.b1bb3be8bc6e959ac300e2a91ec7a20c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7210fd48661991eac38e",
+    "url": "/planning-poker/static/js/3.7210fd48.chunk.js"
+  },
+  {
+    "revision": "8952f0d782a45b10b413",
+    "url": "/planning-poker/static/js/0.8952f0d7.chunk.js"
+  },
+  {
+    "revision": "f97d7a090257e04ec3bd",
+    "url": "/planning-poker/static/js/main.f97d7a09.chunk.js"
+  },
+  {
+    "revision": "7546d6a6860d9264f9ba",
+    "url": "/planning-poker/static/js/runtime~main.7546d6a6.js"
+  },
+  {
+    "revision": "3a80e5d8ee86ad7eae0c",
+    "url": "/planning-poker/static/js/2.3a80e5d8.chunk.js"
+  },
+  {
+    "revision": "a0d405c446c2e2de9737",
+    "url": "/planning-poker/static/js/5.a0d405c4.chunk.js"
+  },
+  {
+    "revision": "2cecd2f4e363e6b08ecf",
+    "url": "/planning-poker/static/js/4.2cecd2f4.chunk.js"
+  },
+  {
+    "revision": "f97d7a090257e04ec3bd",
+    "url": "/planning-poker/static/css/main.945900dd.chunk.css"
+  },
+  {
+    "revision": "2cecd2f4e363e6b08ecf",
+    "url": "/planning-poker/static/css/4.fc3f452b.chunk.css"
+  },
+  {
+    "revision": "7210fd48661991eac38e",
+    "url": "/planning-poker/static/css/3.3495178d.chunk.css"
+  },
+  {
+    "revision": "3a80e5d8ee86ad7eae0c",
+    "url": "/planning-poker/static/css/2.32b58e44.chunk.css"
+  },
+  {
+    "revision": "7e962e6b7519975e50449026fc4b5f25",
+    "url": "/planning-poker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/planning-poker/precache-manifest.3db5a2a7a48c1bb802cd5a2a11b75805.js"
+  "/planning-poker/precache-manifest.b1bb3be8bc6e959ac300e2a91ec7a20c.js"
 );
 
 workbox.clientsClaim();
